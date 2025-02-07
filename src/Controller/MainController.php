f447c6863f41571203c07b557ab68330db2ac585
arjones <danielarjones9@gmail.com>
2025-02-07 20:10:45 +0100

    add repositorio

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,26 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/','home')]
-    public function homepage(): Response
+    #[Route('/')]
+    public function homepage(StarshipRepository $starshipRepository): Response
     {
-        $starshipCount = 457;
-        $myShip = [
-            'name' => 'USS LeafyCruiser (NCC-0001)',
-            'class' => 'Garden',
-            'captain' => 'Jean-Luc Pickles',
-            'status' => 'under construction',
-        ];
+        $ships = $starshipRepository->findAll();
+        $myShip = $ships[array_rand($ships)];
 
         return $this->render('main/homepage.html.twig', [
-            'numberOfStarships' => $starshipCount,
             'myShip' => $myShip,
+            'ships' => $ships,
         ]);
     }
 }
