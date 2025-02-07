ece56ea920f8c07dd9d5b98c66213b88e2e33a65
arjones <danielarjones9@gmail.com>
2025-02-07 17:54:57 +0100

    add home

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,17 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class MainController extends AbstractController
+class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): JsonResponse
+    #[Route('/')]
+    public function homepage(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
+        $starshipCount = 457;
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
         ]);
     }
 }
